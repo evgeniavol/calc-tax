449d748483cc63a9cb85216f5149a8bd476d46e3
Evgenia <evgeniavolkova120@gmail.com>
2022-10-13 16:03:59 +0300

    calculator tax

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
+const formatCurrency = n =>
+    new Intl.NumberFormat('ru-RU', {
+        style: 'currency',
+        currency: 'RUB',
+        maximumFractionDigits: 2,
+    }).format(n);
+
 const navigationLinks = document.querySelectorAll('.navigation__link');
 const calcElems = document.querySelectorAll('.calc');
 
@@ -5,17 +12,67 @@ for (let i = 0; i < navigationLinks.length; i++) {
     navigationLinks[i].addEventListener('click', (e) => {
         e.preventDefault();
         for (let j = 0; j < calcElems.length; j++) {
-            if(navigationLinks[i].dataset.tax === calcElems[j].dataset.tax) {
+            if (navigationLinks[i].dataset.tax === calcElems[j].dataset.tax) {
                 calcElems[j].classList.add('calc_active');
                 navigationLinks[i].classList.add('navigation__link_active');
-            }
-            else {
+            } else {
                 calcElems[j].classList.remove('calc_active');
                 navigationLinks[j].classList.remove('navigation__link_active');
             }
         }
     })
 }
+
+// Самозанятый и ИП НПД
+const selfEmployment = document.querySelector('.self-employment');
+const formSelfEmployment = selfEmployment.querySelector('.calc__form');
+const resultTaxSelfEmployment = selfEmployment.querySelector('.result__tax');
+const calcCompensation = selfEmployment.querySelector('.calc__label_compensation');
+const resultBlockCompensation = selfEmployment.querySelectorAll('.result__block_compensation');
+const resultTaxCompensation = selfEmployment.querySelector('.result__tax_compensation');
+const resultTaxRestCompensation = selfEmployment.querySelector('.result__tax_rest-compensation');
+const resultTaxResult = selfEmployment.querySelector('.result__tax_result');
+
+
+
+
+const checkCompenstation = () => {
+    const setDisplay = formSelfEmployment.addCompensation.checked ? 'block' : 'none';
+    calcCompensation.style.display = setDisplay;
+    resultBlockCompensation.forEach((elem) => {
+        elem.style.display = setDisplay;
+    })
+}
+checkCompenstation();
+
+
+formSelfEmployment.addEventListener('input', () => {
+    const resIndividual = formSelfEmployment.individual.value * 0.04;
+    const resEntity = formSelfEmployment.entity.value * 0.06;
+
+    checkCompenstation();
+
+    const tax = resIndividual + resEntity
+    formSelfEmployment.compensation.value =
+        formSelfEmployment.compensation.value > 10000 ?
+        10000 :
+        formSelfEmployment.compensation.value;
+    const benefit = formSelfEmployment.compensation.value;
+    const resBenefit = formSelfEmployment.individual.value * 0.01 +
+        formSelfEmployment.entity.value * 0.02;
+    const finalBenefit = benefit - resBenefit > 0 ? benefit - resBenefit : 0;
+    const finalTax = tax - (benefit - finalBenefit);
+
+    resultTaxSelfEmployment.textContent = formatCurrency(tax);
+    resultTaxCompensation.textContent = formatCurrency(benefit - finalBenefit);
+    resultTaxRestCompensation.textContent = formatCurrency(finalBenefit);
+    resultTaxResult.textContent = formatCurrency(finalTax);
+});
+
+
+
+
+// AUSN
 const ausn = document.querySelector('.ausn');
 const formAusn = ausn.querySelector('.calc__form');
 const resultTaxTotal = ausn.querySelector('.result__tax_total');
@@ -23,15 +80,15 @@ const calcLabelExpenses = ausn.querySelector('.calc__label_expenses');
 
 calcLabelExpenses.style.display = 'none';
 
-formAusn.addEventListener('input', ()=> {
-    if(formAusn.type.value === 'income') {
+formAusn.addEventListener('input', () => {
+    if (formAusn.type.value === 'income') {
         calcLabelExpenses.style.display = 'none';
-        resultTaxTotal.textContent = formAusn.income.value * 0.08;
-        formAusn.expenses.value ='';
+        resultTaxTotal.textContent = formatCurrency(formAusn.income.value * 0.08);
+        formAusn.expenses.value = '';
     }
-    if(formAusn.type.value === 'expenses') {
+    if (formAusn.type.value === 'expenses') {
         calcLabelExpenses.style.display = 'block';
-        resultTaxTotal.textContent = (formAusn.income.value - formAusn.expenses.value) * 0.2;
+        resultTaxTotal.textContent = formatCurrency((formAusn.income.value - formAusn.expenses.value) * 0.2);
     }
- 
+
 });
