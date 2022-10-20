a9744edac74e144a89c2a37fae380f27739d59df
Evgenia <evgeniavolkova120@gmail.com>
2022-10-20 16:16:07 +0300

    calculator tax

--- script/script.js
+++ script/script.js
@@ -1,52 +1,53 @@
 const formatCurrency = n =>
-    new Intl.NumberFormat('ru-RU', {
-        style: 'currency',
-        currency: 'RUB',
-        maximumFractionDigits: 2,
-    }).format(n);
+  new Intl.NumberFormat('ru-RU', {
+    style: 'currency',
+    currency: 'RUB',
+    maximumFractionDigits: 2,
+  }).format(n);
 
-const navigationLinks = document.querySelectorAll('.navigation__link');
-const calcElems = document.querySelectorAll('.calc');
+{
+  const navigationLinks = document.querySelectorAll('.navigation__link');
+  const calcElems = document.querySelectorAll('.calc');
 
-for (let i = 0; i < navigationLinks.length; i++) {
+  for (let i = 0; i < navigationLinks.length; i++) {
     navigationLinks[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        for (let j = 0; j < calcElems.length; j++) {
-            if (navigationLinks[i].dataset.tax === calcElems[j].dataset.tax) {
-                calcElems[j].classList.add('calc_active');
-                navigationLinks[i].classList.add('navigation__link_active');
-            } else {
-                calcElems[j].classList.remove('calc_active');
-                navigationLinks[j].classList.remove('navigation__link_active');
-            }
+      e.preventDefault();
+      for (let j = 0; j < calcElems.length; j++) {
+        if (navigationLinks[i].dataset.tax === calcElems[j].dataset.tax) {
+          calcElems[j].classList.add('calc_active');
+          navigationLinks[i].classList.add('navigation__link_active');
+        } else {
+          calcElems[j].classList.remove('calc_active');
+          navigationLinks[j].classList.remove('navigation__link_active');
         }
+      }
     })
+  }
 }
 
 // Самозанятый и ИП НПД
-const selfEmployment = document.querySelector('.self-employment');
-const formSelfEmployment = selfEmployment.querySelector('.calc__form');
-const resultTaxSelfEmployment = selfEmployment.querySelector('.result__tax');
-const calcCompensation = selfEmployment.querySelector('.calc__label_compensation');
-const resultBlockCompensation = selfEmployment.querySelectorAll('.result__block_compensation');
-const resultTaxCompensation = selfEmployment.querySelector('.result__tax_compensation');
-const resultTaxRestCompensation = selfEmployment.querySelector('.result__tax_rest-compensation');
-const resultTaxResult = selfEmployment.querySelector('.result__tax_result');
+{
+  const selfEmployment = document.querySelector('.self-employment');
+  const formSelfEmployment = selfEmployment.querySelector('.calc__form');
+  const resultTaxSelfEmployment = selfEmployment.querySelector('.result__tax');
+  const calcCompensation = selfEmployment.querySelector('.calc__label_compensation');
+  const resultBlockCompensation = selfEmployment.querySelectorAll('.result__block_compensation');
+  const resultTaxCompensation = selfEmployment.querySelector('.result__tax_compensation');
+  const resultTaxRestCompensation = selfEmployment.querySelector('.result__tax_rest-compensation');
+  const resultTaxResult = selfEmployment.querySelector('.result__tax_result');
 
 
-
-
-const checkCompenstation = () => {
-    const setDisplay = formSelfEmployment.addCompensation.checked ? 'block' : 'none';
+  const checkCompenstation = () => {
+    const setDisplay = formSelfEmployment.addCompensation.checked ? '' : 'none';
     calcCompensation.style.display = setDisplay;
     resultBlockCompensation.forEach((elem) => {
-        elem.style.display = setDisplay;
+      elem.style.display = setDisplay;
     })
-}
-checkCompenstation();
+  }
+  checkCompenstation();
 
 
-formSelfEmployment.addEventListener('input', () => {
+  formSelfEmployment.addEventListener('input', () => {
     const resIndividual = formSelfEmployment.individual.value * 0.04;
     const resEntity = formSelfEmployment.entity.value * 0.06;
 
@@ -54,12 +55,12 @@ formSelfEmployment.addEventListener('input', () => {
 
     const tax = resIndividual + resEntity
     formSelfEmployment.compensation.value =
-        formSelfEmployment.compensation.value > 10000 ?
-        10000 :
-        formSelfEmployment.compensation.value;
+      formSelfEmployment.compensation.value > 10000 ?
+      10000 :
+      formSelfEmployment.compensation.value;
     const benefit = formSelfEmployment.compensation.value;
     const resBenefit = formSelfEmployment.individual.value * 0.01 +
-        formSelfEmployment.entity.value * 0.02;
+      formSelfEmployment.entity.value * 0.02;
     const finalBenefit = benefit - resBenefit > 0 ? benefit - resBenefit : 0;
     const finalTax = tax - (benefit - finalBenefit);
 
@@ -67,28 +68,179 @@ formSelfEmployment.addEventListener('input', () => {
     resultTaxCompensation.textContent = formatCurrency(benefit - finalBenefit);
     resultTaxRestCompensation.textContent = formatCurrency(finalBenefit);
     resultTaxResult.textContent = formatCurrency(finalTax);
-});
-
+  });
+}
 
 
 
 // AUSN
-const ausn = document.querySelector('.ausn');
-const formAusn = ausn.querySelector('.calc__form');
-const resultTaxTotal = ausn.querySelector('.result__tax_total');
-const calcLabelExpenses = ausn.querySelector('.calc__label_expenses');
+{
+  const ausn = document.querySelector('.ausn');
+  const formAusn = ausn.querySelector('.calc__form');
+  const resultTaxTotal = ausn.querySelector('.result__tax_total');
+  const calcLabelExpenses = ausn.querySelector('.calc__label_expenses');
 
-calcLabelExpenses.style.display = 'none';
+  calcLabelExpenses.style.display = 'none';
 
-formAusn.addEventListener('input', () => {
+  formAusn.addEventListener('input', () => {
     if (formAusn.type.value === 'income') {
-        calcLabelExpenses.style.display = 'none';
-        resultTaxTotal.textContent = formatCurrency(formAusn.income.value * 0.08);
-        formAusn.expenses.value = '';
+      calcLabelExpenses.style.display = 'none';
+      resultTaxTotal.textContent = formatCurrency(formAusn.income.value * 0.08);
+      formAusn.expenses.value = '';
     }
     if (formAusn.type.value === 'expenses') {
-        calcLabelExpenses.style.display = 'block';
-        resultTaxTotal.textContent = formatCurrency((formAusn.income.value - formAusn.expenses.value) * 0.2);
+      calcLabelExpenses.style.display = '';
+      resultTaxTotal.textContent = formatCurrency((formAusn.income.value - formAusn.expenses.value) * 0.2);
     }
 
-});
+  });
+}
+// ОСНО
+{
+  const osno = document.querySelector('.osno');
+  const formOsno = osno.querySelector('.calc__form');
+
+  const ndflExpenses = osno.querySelector('.result__block_ndfl-expenses');
+  const ndflIncome = osno.querySelector('.result__block_ndfl-income');
+  const profit = osno.querySelector('.result__block_profit');
+
+  const resultTaxNds = osno.querySelector('.result__tax_nds');
+  const resultTaxProperty = osno.querySelector('.result__tax_property');
+  const resultTaxNdflExpenses = osno.querySelector('.result__tax_ndfl-expenses');
+  const resultTaxNdflIncome = osno.querySelector('.result__tax_ndfl-income');
+  const resultTaxProfit = osno.querySelector('.result__tax_profit');
+
+
+  const checkFormBusiness = () => {
+    if (formOsno.formBusiness.value === 'ИП') {
+      ndflExpenses.style.display = '';
+      ndflIncome.style.display = '';
+      profit.style.display = 'none';
+    }
+    if (formOsno.formBusiness.value === 'ООО') {
+      ndflExpenses.style.display = 'none';
+      ndflIncome.style.display = 'none';
+      profit.style.display = '';
+    }
+  }
+  checkFormBusiness();
+
+  formOsno.addEventListener('input', () => {
+    checkFormBusiness();
+    const income = formOsno.income.value;
+    const expenses = formOsno.expenses.value;
+    const property = formOsno.property.value;
+
+    const nds = income * 0.2;
+    const taxProperty = property * 0.02;
+    const profit = income - expenses;
+    const ndflExpensesTotal = profit * 0.13;
+    const ndflIncomeTotal = profit * 0.2;
+
+    resultTaxNds.textContent = nds;
+    resultTaxProperty.textContent = taxProperty;
+    resultTaxNdflExpenses.textContent = ndflExpensesTotal;
+    resultTaxNdflIncome.textContent = ndflIncomeTotal;
+    resultTaxProfit.textContent = taxProperty;
+
+  });
+}
+// УСН 
+
+{
+  const LIMIT = 300_000;
+  const usn = document.querySelector('.usn');
+  const formUsn = usn.querySelector('.calc__form');
+
+  const calcLabelExpenses = usn.querySelector('.calc__label_expenses');
+  const calcLabelProperty = usn.querySelector('.calc__label_property');
+  const resultBlockProperty = usn.querySelector('.result__block_property');
+
+
+  const resultTaxTotal = usn.querySelector('.result__tax_total');
+  const resultTaxProperty = usn.querySelector('.result__tax_property');
+
+  const checkShopProperty = (typeTax) => {
+    switch (typeTax) {
+      case 'income': {
+        calcLabelExpenses.style.display = 'none';
+        calcLabelProperty.style.display = 'none';
+        resultBlockProperty.style.display = 'none';
+
+        formUsn.expenses.value = '';
+        formUsn.property.value = '';
+        break;
+      };
+    case 'ip-expenses': {
+      calcLabelExpenses.style.display = '';
+      calcLabelProperty.style.display = 'none';
+      resultBlockProperty.style.display = 'none';
+      formUsn.property.value = '';
+
+      break;
+    };
+    case 'ooo-expenses': {
+      calcLabelExpenses.style.display = '';
+      calcLabelProperty.style.display = '';
+      resultBlockProperty.style.display = '';
+      break;
+    };
+    }
+  }
+
+  const typeTax = {
+    'income': () => {
+      calcLabelExpenses.style.display = 'none';
+      calcLabelProperty.style.display = 'none';
+      resultBlockProperty.style.display = 'none';
+
+      formUsn.expenses.value = '';
+      formUsn.property.value = '';
+    },
+    'ip-expenses': () => {
+      calcLabelExpenses.style.display = '';
+      calcLabelProperty.style.display = 'none';
+      resultBlockProperty.style.display = 'none';
+      formUsn.property.value = '';
+    },
+    'ooo-expenses': () => {
+      calcLabelExpenses.style.display = '';
+      calcLabelProperty.style.display = '';
+      resultBlockProperty.style.display = '';
+    },
+  }
+
+  const percent = {
+    'income': 0.06,
+    'ip-expenses': 0.15,
+    'ooo-expenses': 0.15,
+  };
+
+  typeTax[formUsn.typeTax.value]();
+
+
+
+  formUsn.addEventListener('input', () => {
+    checkShopProperty(formUsn.typeTax.value);
+
+    const income = formUsn.income.value;
+    const expenses = formUsn.expenses.value;
+    const contributions = formUsn.contributions.value;
+    const property = formUsn.property.value;
+
+    let profit = income - contributions;
+
+    if (formUsn.typeTax.value !== 'income') {
+      profit -= expenses;
+    }
+
+    const taxBigIncome = income > LIMIT ? (profit - LIMIT) * 0.01 : 0;
+    const sum = profit - (taxBigIncome < 0 ? 0 : taxBigIncome);
+    const tax = sum * percent[formUsn.typeTax.value];
+    const taxProperty = property * 0.02;
+
+    resultTaxTotal.textContent = formatCurrency(tax);
+    resultTaxProperty.textContent = formatCurrency(taxProperty);
+  });
+
+}
